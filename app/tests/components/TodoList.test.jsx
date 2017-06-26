4a0add6fc1db0352e8b91db02711f60432507461
Rudolf Winestock <rudolf@winestockwebdesign.com>
2017-06-26 16:14:38 -0400

    Test search component

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -12,17 +12,17 @@ describe('TodoList', () => {
     expect(TodoList).toExist();
   });
 
-  // it('should render one Todo component for each todo item', () => {
-  //   var todos: [{
-  //     id: 1,
-  //     text: 'Do something'
-  //   }, {
-  //     id: 2,
-  //     text: 'Check mail'
-  //   }];
-  //   var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-  //   var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
-  //
-  //   expect(todosComponents.length).toBe(todos.length);
-  // });
+  it('should render one Todo component for each todo item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Do something'
+    }, {
+      id: 2,
+      text: 'Check mail'
+    }];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
+  });
 });
