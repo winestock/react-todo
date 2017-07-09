27d6c92a12383fb9eedb0555c97f7fa6d510d3e4
Rudolf Winestock <rudolf@winestockwebdesign.com>
2017-07-08 22:18:10 -0400

    Fetch todos from Firebase on app start

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -90,15 +90,19 @@ describe('Actions', () => {
     var testTodoRef;
 
     beforeEach((done) => {
-      testTodoRef = firebaseRef.child('todos').push();
+      var todosRef = firebaseRef.child('todos');
 
-      testTodoRef.set({
-        text: 'Something to do',
-        completed: false,
-        completedAt: 23453453
+      todosRef.remove().then(() => {
+        testTodoRef = firebaseRef.child('todos').push();
+
+        return testTodoRef.set({
+          text: 'Something to do',
+          completed: false,
+          completedAt: 23453453
+        })
       }).then(() => {
-        done();
-      });
+              done();
+            }).catch(done);
     });
 
     afterEach((done) => {
@@ -116,7 +120,7 @@ describe('Actions', () => {
 
         expect(mockActions[0]).toInclude({
           type: 'UPDATE_TODO',
-          id: testTodoRef.key,
+          id: testTodoRef.key
         });
 
         expect(mockActions[0].updates).toInclude({
@@ -127,6 +131,21 @@ describe('Actions', () => {
 
         done();
       }, done());
+    });
+
+    it('should populate todos and dispatch ADD_TODOS', (done) => {
+      const store = createMockStore({});
+      const action = actions.startAddTodos();
+
+      store.dispatch(action).then(() => {
+        const mockActions = store.getActions();
+
+        expect(mockActions[0].type).toEqual('ADD_TODOS');
+        expect(mockActions[0].todos.length).toEqual(1);
+        expect(mockActions[0].todos[0].text).toEqual('Something to do');
+
+        done();
+      }, done)
     });
   });
 });
